90109011aa1e6b39b381d7ced617e4012e19c8cb
Virgil Barnard <virgil.barnard@gmail.com>
2025-07-08 09:46:42 -0400

    More cleanup

--- dieselwolf/models/__init__.py
+++ dieselwolf/models/__init__.py
@@ -4,8 +4,6 @@ from .lightning_module import AMRClassifier
 from .complex_transformer import ComplexTransformerEncoder
 from .configurable_cnn import ConfigurableCNN
 from .configurable_mobile_rat import ConfigurableMobileRaT
-from .mobile_rat import MobileRaT
-from .nmformer import NMformer
 from .moco_v3 import MoCoV3
 from .factory import build_backbone
 
@@ -13,8 +11,6 @@ __all__ = [
     "AMRClassifier",
     "ComplexTransformerEncoder",
     "ConfigurableMobileRaT",
-    "MobileRaT",
-    "NMformer",
     "ConfigurableCNN",
     "MoCoV3",
     "build_backbone",
